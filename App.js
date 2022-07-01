268efcb4f83b25282d45600daa8ba18ffbeabdff
Ignatius <igee54077@gmail.com>
2022-07-01 15:00:34 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import  React from 'react'
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import OnBoarding from './app/screens/OnBoarding/OnBoarding'
+const Stack = createStackNavigator()
+export default function App() {
+  return (
+<NavigationContainer>
+  <Stack.Navigator>
+    <Stack.Screen name='OnBoarding' component={OnBoarding} options={{headerShown:false}} />
+  </Stack.Navigator>
+</NavigationContainer>
+  )
+}
